988a05dc3cd46a95367e5ccb7ce15936e79d9da9
farhanjiratullah <farhanjiratullah794@gmail.com>
2023-09-07 21:16:10 +0700

    feat: Integrate store role

--- pages/dashboard/roles/create.vue
+++ pages/dashboard/roles/create.vue
@@ -4,9 +4,47 @@
     });
 
     useHead({
-        title: "Create Team | PowerHuman",
-        meta: [{ name: "description", content: "Create team page" }],
+        title: "Create Role | PowerHuman",
+        meta: [{ name: "description", content: "Create role page" }],
     });
+
+    const accessToken = useCookie("accessToken");
+
+    const { data: companies, pending } = await useLazyFetch(
+        `${useRuntimeConfig().public.apiBase}/companies/all`,
+        {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken.value}`,
+            },
+            transform: (companies) => companies.data,
+        }
+    );
+
+    const form = ref({
+        company_id: "",
+        name: "",
+        responsibilities: [
+            {
+                name: "",
+            },
+        ],
+    });
+
+    const errors = ref({});
+
+    const { store, isLoading } = useRoles();
+
+    const addResponsibility = () => {
+        form.value.responsibilities.push({
+            name: "",
+        });
+    };
+
+    const removeResponsibility = (index) => {
+        form.value.responsibilities.splice(index, 1);
+    };
 </script>
 
 <template>
@@ -14,51 +52,136 @@
         <section
             class="py-[70px] flex flex-col items-center justify-center px-4"
         >
-            <div class="text-[32px] font-semibold text-dark">
-                Build New Team
-            </div>
+            <div class="text-[32px] font-semibold text-dark">New Role</div>
             <p class="mt-4 text-base leading-7 text-center mb-[50px] text-grey">
-                Team that can bring your company <br />
-                growing bigger and bigger
+                Manage your employees to achieve <br />
+                a bigger goals for your company
             </p>
-            <form class="w-full card">
-                <div class="mb-[2px] mx-auto">
-                    <img src="/svgs/ric-box.svg" alt="" />
+            <form class="w-full card" @submit.prevent="store(form, errors)">
+                <div class="form-group">
+                    <label for="company_id" class="text-grey"
+                        >Company <span class="text-red-500">*</span></label
+                    >
+                    <select
+                        name="company_id"
+                        id="company_id"
+                        class="appearance-none input-field form-icon-chevron_down"
+                        v-model="form.company_id"
+                    >
+                        <option v-if="pending">Loading...</option>
+                        <template v-else>
+                            <option
+                                v-for="(company, index) in companies"
+                                :key="company.id"
+                                :value="company.id"
+                            >
+                                {{ company.name }}
+                            </option>
+                        </template>
+                    </select>
+                    <div v-if="errors?.value?.company_id">
+                        <p class="text-red-500">
+                            {{ errors?.value?.company_id[0] }}
+                        </p>
+                    </div>
                 </div>
                 <div class="form-group">
-                    <label for="" class="text-grey">Email Address</label>
-                    <input
-                        type="email"
-                        class="input-field disabled:bg-grey disabled:outline-none"
-                        value="angga@yourcompany.com"
-                        disabled
-                    />
-                </div>
-                <div class="form-group">
-                    <label for="" class="text-grey">Team Name</label>
+                    <label for="name" class="text-grey"
+                        >Role Name <span class="text-red-500">*</span></label
+                    >
                     <input
                         type="text"
                         class="input-field"
-                        value="Growth Marketing"
+                        id="name"
+                        name="name"
+                        v-model="form.name"
                     />
+                    <div v-if="errors?.value?.name">
+                        <p class="text-red-500">
+                            {{ errors?.value?.name[0] }}
+                        </p>
+                    </div>
                 </div>
                 <div class="form-group">
-                    <label for="" class="text-grey">Status</label>
-                    <select
-                        name=""
-                        id=""
-                        class="appearance-none input-field form-icon-chevron_down"
-                    >
-                        <option value="" selected>Active</option>
-                        <option value="">Inactive</option>
-                    </select>
+                    <label for="responsibilities-0" class="text-grey">
+                        Responsibility <span class="text-red-500">*</span>
+                    </label>
+                    <ul id="listResp" class="flex flex-col gap-4">
+                        <li
+                            class="flex flex-col gap-2"
+                            v-for="(
+                                responsibility, index
+                            ) in form.responsibilities"
+                            :key="index"
+                        >
+                            <div class="flex items-center gap-5">
+                                <input
+                                    type="text"
+                                    :id="`responsibilities-${index}`"
+                                    :name="`responsibilities-${index}`"
+                                    v-model="form.responsibilities[index].name"
+                                    class="w-full input-field"
+                                />
+                                <div>
+                                    <template
+                                        v-if="
+                                            index ===
+                                            form.responsibilities.length - 1
+                                        "
+                                    >
+                                        <a
+                                            @click.prevent="addResponsibility"
+                                            role="button"
+                                            :id="`addResponsibility-${index}`"
+                                        >
+                                            <img
+                                                src="/svgs/ric-plus.svg"
+                                                alt="Add Responsibility"
+                                            />
+                                        </a>
+                                    </template>
+                                    <template v-else>
+                                        <a
+                                            @click.prevent="
+                                                removeResponsibility(index)
+                                            "
+                                            role="button"
+                                            :id="`removeResponsibility-${index}`"
+                                        >
+                                            <img
+                                                src="/svgs/ric-close-red.svg"
+                                                alt="Remove Responsibility"
+                                            />
+                                        </a>
+                                    </template>
+                                </div>
+                            </div>
+                            <div
+                                v-if="
+                                    errors?.value?.[
+                                        `responsibilities.${index}.name`
+                                    ]
+                                "
+                            >
+                                <p class="text-red-500">
+                                    {{
+                                        errors?.value?.[
+                                            `responsibilities.${index}.name`
+                                        ][0]
+                                    }}
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-                <a
-                    href="my_teams.html"
-                    class="w-full btn btn-primary mt-[14px]"
+
+                <button
+                    type="submit"
+                    class="w-full btn btn-primary mt-[14px] disabled:opacity-50 disabled:cursor-not-allowed"
+                    :disabled="isLoading"
                 >
-                    Continue
-                </a>
+                    {{ isLoading ? "Loading..." : "Continue" }}
+                </button>
             </form>
         </section>
     </div>
